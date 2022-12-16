64b4620f375a2465587d8d8d12637805961ea864
rika kaminoyama <richun.06@gmail.com>
2022-12-17 02:39:18 +0900

    文字列で渡すため削除

--- app/models/care_user.rb
+++ app/models/care_user.rb
@@ -1,7 +1,7 @@
 class CareUser < ApplicationRecord
   enum sex: { 男:0, 女:1}
   enum availability: { あり:0, なし:1}
-  enum division: { L宿泊:0, S宿泊:1, 通所:2, 訪問:3, その他:4}
+  # enum division: { L宿泊:0, S宿泊:1, 通所:2, 訪問:3, その他:4}
 
   scope :name_search, -> (name) { where("name LIKE ?", "%#{name}%") if name.present? }
   scope :sex_search, -> (sex) { where(sex: sex) if sex.present? }
