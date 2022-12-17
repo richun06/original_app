145e7b41c3e6e55aee6baa0b5d24b16287e72a5a
rika kaminoyama <richun.06@gmail.com>
2022-12-17 19:51:12 +0900

    施設idを持ったseedデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name: "テスト花子", email: "hanako@sample.com", password: "111111", password_confirmation: "111111", admin: false)
 User.create!(name: "テスト管理者", email: "admin@sample.com", password: "111111", password_confirmation: "111111", admin: true)
-CareUser.create!(name: "テスト太郎", birthday: '1950-01-01', age: 72, sex: 1, availability: 1, division: 1, user_id: 1)
+User.create!(name: "施設", email: "shisetsu@sample.com", password: "111111", password_confirmation: "111111", admin: false, owner_id: 1)
+User.create!(name: "テスト花子", email: "hanako@sample.com", password: "111111", password_confirmation: "111111", admin: false)
+CareUser.create!(name: "テスト太郎", birthday: '1950-01-01', age: 72, sex: 1, availability: 1, division: 1, user_id: 3)
