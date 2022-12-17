323060e1e0a85b860af6f7ba2b385c583e77c412
rika kaminoyama <richun.06@gmail.com>
2022-12-17 22:11:57 +0900

    電話番号、郵便番号のバリデーション復活

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,9 +29,9 @@ class User < ApplicationRecord
   has_many :care_users
   has_many :reserves
 
-  # VALID_PHONE_NUMBER_REGEX = /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0[-]?\d{4}[-]?\d{4}\z/
-  # validates :phone_number, format: { with: VALID_PHONE_NUMBER_REGEX }
+  VALID_PHONE_NUMBER_REGEX = /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0[-]?\d{4}[-]?\d{4}\z/
+  validates :phone_number, format: { with: VALID_PHONE_NUMBER_REGEX }
 
-  # VALID_POST_CODE_REGEX = /\A\d{3}[-]?\d{4}\z/
-  # validates :postcode, format: { with: VALID_POST_CODE_REGEX }
+  VALID_POST_CODE_REGEX = /\A\d{3}[-]?\d{4}\z/
+  validates :postcode, format: { with: VALID_POST_CODE_REGEX }
 end
