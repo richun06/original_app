c576a3810655a1b3e650ad12eeeac368507008ff
rika kaminoyama <richun.06@gmail.com>
2022-12-17 19:50:22 +0900

    施設に付与するカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_16_174558) do
+ActiveRecord::Schema.define(version: 2022_12_17_094013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2022_12_16_174558) do
     t.string "address_city"
     t.string "address_street"
     t.string "address_building"
+    t.integer "owner_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
