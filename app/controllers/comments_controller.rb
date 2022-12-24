4ece18d8dc55eba3eba3aee6422d4cb635e6b183
rika kaminoyama <richun.06@gmail.com>
2022-12-25 01:13:56 +0900

    コメントの編集機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :set_health, only: [:create, :edit, :update]
 
   def create
-    @health = Health.find(params[:health_id])
     @comment = @health.comments.build(comment_params)
 
     respond_to do |format|
@@ -13,9 +13,34 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @comment = @health.comments.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
+    end
+  end
+
+  def update
+    @comment = @health.comments.find(params[:id])
+      respond_to do |format|
+        if @comment.update(comment_params)
+          flash.now[:notice] = 'コメントが編集されました'
+          format.js { render :index }
+        else
+          flash.now[:notice] = 'コメントの編集に失敗しました'
+          format.js { render :edit }
+        end
+      end
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def set_health
+    @health = Health.find(params[:health_id])
   end
 end
