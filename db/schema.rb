5b022eb1a14d5abe93a0a6aeae3f751ad693b748
rika kaminoyama <richun.06@gmail.com>
2022-12-26 01:55:49 +0900

    責任者テーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_25_143830) do
+ActiveRecord::Schema.define(version: 2022_12_25_165254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,12 @@ ActiveRecord::Schema.define(version: 2022_12_25_143830) do
     t.bigint "care_user_id"
     t.text "transfer"
     t.index ["care_user_id"], name: "index_healths_on_care_user_id"
+  end
+
+  create_table "managers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "reserves", force: :cascade do |t|
