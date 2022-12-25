f9a6b153acc026bd76a3acaa157a9948f6ed7016
rika kaminoyama <richun.06@gmail.com>
2022-12-25 23:39:52 +0900

    UserモデルとCommentモデルを関連づけるため外部キーを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_24_154330) do
+ActiveRecord::Schema.define(version: 2022_12_25_143830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,9 @@ ActiveRecord::Schema.define(version: 2022_12_24_154330) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
     t.index ["health_id"], name: "index_comments_on_health_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "healths", force: :cascade do |t|
@@ -110,6 +112,7 @@ ActiveRecord::Schema.define(version: 2022_12_24_154330) do
 
   add_foreign_key "care_users", "users"
   add_foreign_key "comments", "healths"
+  add_foreign_key "comments", "users"
   add_foreign_key "healths", "care_users"
   add_foreign_key "reserves", "users"
 end
