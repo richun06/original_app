1fe28a9dbac876efc492f8713278dd51ee3c6e5c
rika kaminoyama <richun.06@gmail.com>
2022-12-26 18:19:50 +0900

    責任者を登録し一覧画面に表示できるよう記述

--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -3,4 +3,23 @@ class ManagersController < ApplicationController
   def new
     @manager = Manager.new
   end
+
+  def create
+    @manager = Manager.new(manager_params)
+    if @manager.save
+      redirect_to managers_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @managers = Manager.all
+  end
+
+  private
+
+  def manager_params
+    params.require(:manager).permit(:name)
+  end
 end
